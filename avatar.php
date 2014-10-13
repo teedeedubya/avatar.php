26095d1bb40fcee00e9c00a620ec1adba82f40fa
tony.welder <tony.welder@intermedix.com>
2014-10-13 00:08:14 -0400

    There was a point in my  life where I didn't care about indentation... those days are long gone...

--- avatar.php
+++ avatar.php
@@ -8,60 +8,66 @@ echo "$calls";
 <script>
 var arms="";
 function AJAX(){
-var xmlHttp;
-try{
-xmlHttp=new XMLHttpRequest();
-return xmlHttp;
+  var xmlHttp;
+  try{
+    xmlHttp=new XMLHttpRequest();
+    return xmlHttp;
+  }
+  catch (e){
+    try{
+      xmlHttp=new ActiveXObject("Msxml2.XMLHTTP");
+      return xmlHttp;
+    }
+    catch (e){
+      try{
+        xmlHttp=new ActiveXObject("Microsoft.XMLHTTP");
+        return xmlHttp;
+      }
+      catch (e){
+      alert("Your browser does not support AJAX!");
+      return false;
+      }
+    }
+  }
 }
-catch (e){
-try{
-xmlHttp=new ActiveXObject("Msxml2.XMLHTTP");
-return xmlHttp;
-}
-catch (e){
-try{
-xmlHttp=new ActiveXObject("Microsoft.XMLHTTP");
-return xmlHttp;
-}
-catch (e){
-alert("Your browser does not support AJAX!");
-return false;
-}
-}
-}
-}
+
 function pF(va,v,l){
-if (l == undefined){
-l = "&";
+  if (l == undefined){
+    l = "&";
+  }
+  return (va+"="+v+l);
 }
-return (va+"="+v+l);
-}
+
 function postData(url, parameters){
-var xmlHttp=AJAX();
-xmlHttp.onreadystatechange =  function(){
-if(xmlHttp.readyState > 0 && xmlHttp.readyState < 4){
-//waiting for server to respond
+  var xmlHttp=AJAX();
+  xmlHttp.onreadystatechange =  function(){
+    if(xmlHttp.readyState > 0 && xmlHttp.readyState < 4){
+      //waiting for server to respond
+      }
+      if (xmlHttp.readyState == 4) {
+        //document.getElementById(divid).innerHTML=xmlHttp.responseText;
+        sdf(xmlHttp.responseText);
+    }
+  }
+
+  xmlHttp.open("POST", url, true);
+  xmlHttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xmlHttp.setRequestHeader("Content-length", parameters.length);
+  xmlHttp.setRequestHeader("Connection", "close");
+  //xmlHttp.setRequestHeader(parameters.length);
+  //xmlHttp.setRequestHeader("close");
+  xmlHttp.send(parameters);
 }
-if (xmlHttp.readyState == 4) {
-//document.getElementById(divid).innerHTML=xmlHttp.responseText;
-sdf(xmlHttp.responseText);
-}
-}
-xmlHttp.open("POST", url, true);
-xmlHttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-xmlHttp.setRequestHeader("Content-length", parameters.length);
-xmlHttp.setRequestHeader("Connection", "close");
-//xmlHttp.setRequestHeader(parameters.length);
-//xmlHttp.setRequestHeader("close");
-xmlHttp.send(parameters);
-}
+
 function sdf(gfd){
-document.getElementById("fuckitwelldoitlive").innerHTML = gfd + arms;
-arms = gfd + arms;
-}
+    document.getElementById("fuckitwelldoitlive").innerHTML = gfd + arms;
+    arms = gfd + arms;
+  }
+
 function dosomethingfancy(){
-postData("avatar.php",(pF("calls","1","&")+pF("themen",document.getElementById("yermom").value,"")));
+  postData("avatar.php",(pF("calls","1","&")+pF("themen",document.getElementById("yermom").value,"")));
 }
+
 </script>
 </head>
 <body>
